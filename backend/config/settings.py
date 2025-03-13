7acd69e4975ba6deb742b0df6d6b54452684b983
Murilo Barboza <murilobarboza04@gmail.com>
2025-03-13 20:34:20 -0300

    Ativando Strict Mode no MySQL

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -91,6 +91,7 @@ DATABASES = {
         'PORT': '3306',
         'OPTIONS': {
             'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
     }
 }
